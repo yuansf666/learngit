7787dd10e209034a157753901d3339cee4a22841
yuansf666 <yuansf666@gmail.com>
2025-10-28 19:11:05 +0800

    初始提交main文件

---
+++ main.py
@@ -0,0 +1,5 @@
+#main.py
+def main():
+    print("Welcome to LearnGit!")
+    print("This is a simple program to demonstrate Git usage.")
+    print("Feel free to modify and experiment with this code.")
